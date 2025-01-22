19c096188c3f7683ff6f5e72c61a8870ca4534f9
sa413x <sa413x@gmail.com>
2025-01-22 20:00:54 +0300

    Added TextEditor

--- kspkg-gui/views/content/text_content_processor.cpp
+++ kspkg-gui/views/content/text_content_processor.cpp
@@ -1,20 +1,42 @@
 #include "text_content_processor.hpp"
 
 #include <imgui.h>
+#include "text_editor/text_editor.hpp"
 
 namespace views {
     void text_content_processor::process( const std::shared_ptr< kspkg::package >& package, const std::shared_ptr< kspkg::file >& file ) {
-        const auto file_content = package->extract_file( file );
-        if ( !file_content )
-            return;
+        static std::map< std::string, TextEditor::LanguageDefinition > ext_map = {
+            { ".html", TextEditor::LanguageDefinition::HTML() },
+            { ".loc", TextEditor::LanguageDefinition::HTML() },
+            { ".js", TextEditor::LanguageDefinition::AngelScript() }, // Temp
+        };
 
-        ImGui::PushStyleColor( ImGuiCol_ChildBg, { 0.06f, 0.06f, 0.06f, 1.f } );
-        ImGui::BeginChild( "FilePreview", ImVec2( 0, 0 ), 0, ImGuiWindowFlags_HorizontalScrollbar );
+        static std::shared_ptr< TextEditor > editor;
+        if ( !editor ) {
+            editor = std::make_shared< TextEditor >();
+            editor->SetReadOnly( true );
+        }
 
-        const std::string file_content_str( file_content->begin(), file_content->end() );
-        ImGui::TextUnformatted( file_content_str.c_str(), file_content_str.c_str() + file_content_str.size() );
+        static std::string old_resource;
 
-        ImGui::EndChild();
-        ImGui::PopStyleColor();
+        if ( old_resource != file->get_name() ) {
+            if ( const auto file_content = package->extract_file( file ) ) {
+                const std::string file_content_str( file_content->begin(), file_content->end() );
+                editor->SetText( file_content_str );
+                editor->SetCursorPosition( {} );
+
+                if ( const auto ext = std::filesystem::path( file->get_name() ).extension().string(); ext_map.contains( ext ) ) {
+                    editor->SetColorizerEnable( true );
+                    editor->SetLanguageDefinition( ext_map[ ext ] );
+                }
+                else {
+                    editor->SetColorizerEnable( false );
+                }
+
+                old_resource = file->get_name();
+            }
+        }
+
+        editor->Render( "TextEditor", {}, true );
     }
 } // namespace views
